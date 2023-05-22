ea26c2265c905bf16bc6838d0d0f31ea94d33cf1
davigzola <davi.gzola@gmail.com>
2023-05-22 17:29:15 -0300

    Version 0.5

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var creditCardName = document.querySelector(".credit-card-form-name input[type=text]");
+var creditCardNumber = document.querySelector(".credit-card-form-number input[type=text]");
+var creditCardExpDateMonth = document.querySelector("#expdate-month");
+var creditCardExpDateYear = document.querySelector("#expdate-year");
+var creditCardCVC = document.querySelector(".credit-card-form-cvc input[type=text]");
+
+var frontCardName = document.querySelector(".front-card-name");
+var frontCardNumber = document.querySelector(".front-card-number");
+var frontCardExpDateMonth = document.querySelector("#front-card-expdate-month");
+var frontCardExpDateYear = document.querySelector("#front-card-expdate-year");
+var backCardCVC = document.querySelector(".back-card-cvc");
+
+creditCardName.addEventListener("input", changeName);
+creditCardNumber.addEventListener("input", changeNumber);
+creditCardExpDateMonth.addEventListener("input", changeMonth);
+creditCardExpDateYear.addEventListener("input", changeYear);
+creditCardCVC.addEventListener("input", changeCVC);
+
+function changeName() {
+    frontCardName.textContent = creditCardName.value;   
+    
+}
+
+function changeNumber() {
+    frontCardNumber.textContent = creditCardNumber.value;
+}
+
+function changeMonth() {
+    frontCardExpDateMonth.textContent = creditCardExpDateMonth.value;
+}
+
+function changeYear() {
+    frontCardExpDateYear.textContent = creditCardExpDateYear.value; 
+}
+
+function changeCVC() {
+    backCardCVC.textContent = creditCardCVC.value;
+}
